7f0b7698fb6e71c495add63deade597c8aa4e97c
Deverson Nunes <deversonnunes10@gmail.com>
2023-11-28 21:33:21 -0300

    arquivos adcionados

---
+++ index.js
@@ -0,0 +1,18 @@
+
+ const itensPerguntas=document.querySelectorAll(".item")
+
+ itensPerguntas.forEach(function (item) {
+    
+     item.addEventListener("click", function () {
+
+
+         const itemAtivoAtual=document.querySelector(".ativo")
+         if (itemAtivoAtual) {
+ itemAtivoAtual.classList.remove("ativo")
+         }
+         item.classList.add("ativo")        
+         console.log(itemAtivoAtual)
+
+        
+     })
+ })
